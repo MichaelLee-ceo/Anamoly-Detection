3d35025b080e396a8c259320e79037c85dedbe16
Michael_HSCC <minglun.c.li@gmail.com>
2023-08-03 02:17:44 +0800

    reversion preprocessing.py for json saving formatting

--- preprocessing.py
+++ preprocessing.py
@@ -11,17 +11,24 @@ args = parser.parse_args()
 
 # find files in the directory
 directories = ['normal', 'abnormal']
-info = []
+info, info_total = [], []
 for directory in directories:
     path = os.path.join(args.video_path, directory)
     files = os.listdir(path)
 
     # create normal and abnormal directory for saving frames
     os.makedirs(os.path.join(args.data_path, directory), exist_ok=True)
+
+    record = {
+        "directory": directory,
+        "total_frame": 0,
+        "files": [],
+    }
     
-    frame_count = 0
+    total_frame = 0
     interval = 1
     for file in files:
+        frame_count = 0
         cap = cv2.VideoCapture(os.path.join(path, file))
         fps = cap.get(cv2.CAP_PROP_FPS)
         print("\n[{} | {}] Fps: {}".format(directory, file, fps))
@@ -32,17 +39,20 @@ for directory in directories:
                 cap.release()
                 break
 
-            if frame_count % interval == 0:
-                cv2.imwrite(os.path.join(args.data_path, directory, str(frame_count) + '.png'), frame)
-                print("[{} | {}] Saving frame: {}".format(directory, file, frame_count))
+            if total_frame % interval == 0:
+                cv2.imwrite(os.path.join(args.data_path, directory, str(total_frame) + '.png'), frame)
+                print("[{} | {}] Saving frame: {}".format(directory, file, total_frame))
 
             frame_count += 1
+            total_frame += 1
 
-        info.append({
-            "directory": directory,
+        record["files"].append({
             "file": file,
             "frame_count": frame_count,
         })
+
+    record["total_frame"] = total_frame
+    info.append(record)
 
 with open(args.video_info_file, 'w+') as f:
     json.dump(info, f, indent=4)
